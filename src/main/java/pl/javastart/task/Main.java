a0b3a007fa9858c8c8bf1990c9eb4d5c806b57fb
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-07 10:26:47 +0100

    Rozwiazanie zadanie2.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -10,6 +10,6 @@ public class Main {
         double price1 = 2.5;
         double price2 = 2.19;
         System.out.println(name1 + " " + producer1 + " " + price1 + "zł");
-        System.out.println(name2 + " " + producer2 +" " + price2 + "zł");
+        System.out.println(name2 + " " + producer2 + " " + price2 + "zł");
     }
 }
